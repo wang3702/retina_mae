03e5b72651af5e7ad8246d26b6739421d3b36ec2
Xiao Wang <xiaowang@Xiaos-MacBook-Pro-2.local>
2022-01-21 00:15:37 -0500

    change size for input

--- main_linprobe.py
+++ main_linprobe.py
@@ -187,7 +187,7 @@ def main(args):
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         transform_val = transforms.Compose([
                 #transforms.Resize(256, interpolation=3),
-                transforms.CenterCrop(224),
+                #transforms.CenterCrop(224),
                 transforms.ToTensor(),
                 transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
         from koniq.koniq_dataset import IQADataset
