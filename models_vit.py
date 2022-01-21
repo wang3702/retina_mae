410b4f9e0978245151108665180f9a58a53de47b
Xiao Wang <xiaowang@Xiaos-MacBook-Pro-2.local>
2022-01-20 22:53:08 -0500

    change size for input

--- models_vit.py
+++ models_vit.py
@@ -60,6 +60,12 @@ def vit_base_patch16(**kwargs):
     return model
 
 
+def vit_base_koniq_patch16(**kwargs):
+    model = VisionTransformer(img_size=(1024,768),
+        patch_size=16, embed_dim=768, depth=12, num_heads=12, mlp_ratio=4, qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6), **kwargs)
+    return model
+
 def vit_large_patch16(**kwargs):
     model = VisionTransformer(
         patch_size=16, embed_dim=1024, depth=24, num_heads=16, mlp_ratio=4, qkv_bias=True,
